13bea4621a4cbe2b1884d171c66987cfbbffd769
Kevin MacNeel <kevjac@gmail.com>
2016-01-19 22:07:35 -0800

    Added Validation

--- TNCVote/Models/AccountViewModels.cs
+++ TNCVote/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
@@ -70,21 +71,54 @@ namespace TNCVote.Models
         public string Name { get; set; }
     }
 
-   
+
+
+    public class YearRangeAttribute : RangeAttribute
+    {
+        public YearRangeAttribute()
+          : base( DateTime.Now.AddYears(-130).Year, DateTime.Now.AddYears(-18).Year)
+        {
+           
+        }
+    }
+
+  
+
+    public class ValidateDayInMonthAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            bool flag = false;
+            RegisterViewModel obj = (RegisterViewModel)value;
+            if (obj.BirthDay > 0 && obj.BirthDay <= DateTime.DaysInMonth(obj.BirthYear, obj.BirthMonth) )
+            {
+                flag = true;
+            }
+            return flag;
+        }
+    }
+
+
 
     public class RegisterViewModel
     {
-      
 
+        public const int year =18;
+
+       
         [Required]
+        [StringLength(40,  ErrorMessage = "First name is too long. Please shorten.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+       
         [Required]
+        [StringLength(40, ErrorMessage = "Last name too long. Please shorten.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid Postal Code")]
         [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
          
@@ -92,19 +126,21 @@ namespace TNCVote.Models
         [Display(Name = "Citizenship Country")]
         public string Country { get; set; }
 
-
+        [Range(1, 31, ErrorMessage = "Please enter valid birth day.")]
+        [ValidateDayInMonth(ErrorMessage = "Day not in inputed. Please enter a day in the month.")]
         [Required]
         [Display(Name = "Birth Day")]
-        public string BirthDay { get; set; }
+        public int BirthDay { get; set; }
 
-
+        [Range(1, 12, ErrorMessage = "Please enter valid birth month.")]
         [Required]
         [Display(Name = "Birth Month")]
-        public string BirthMonth { get; set; }
+        public int BirthMonth { get; set; }
 
+        [YearRange(ErrorMessage = "Please enter valid birth year. You must be over 18 years old to register.")]
         [Required]
         [Display(Name = "Birth Year")]
-        public string BirthYear { get; set; }
+        public int BirthYear { get; set; }
 
         [Required]
         [EmailAddress]
@@ -122,42 +158,56 @@ namespace TNCVote.Models
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
+        [MaxLength(10, ErrorMessage = "Please select a valid city.")]
         [Display(Name = "City of Residence")]
         public string City { get; set; }
 
+        [MaxLength(10, ErrorMessage = "Please select a valid county.")]
         [Display(Name = "County of Residence")]
         public string County { get; set; }
 
+        [MaxLength(10, ErrorMessage = "Please select a valid sex.")]
         [Display(Name = "Sex")]
         public string Sex { get; set; }
 
+        [MaxLength(40, ErrorMessage = "Please select a valid income range.")]
         [Display(Name = "Income Range")]
         public string IncomeRange { get; set; }
 
+        [MaxLength(40, ErrorMessage = "Please select a valid educational attainment.")]
         [Display(Name = "Education")]
         public string Education { get; set; }
 
+        [MaxLength(40, ErrorMessage = "Please select a valid marital status.")]
         [Display(Name = "Married")]
         public string Married { get; set; }
 
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
         public string PhoneNumber { get; set; }
 
+        [Url]
         [Display(Name = "Twitter")]
         public string Twitter { get; set; }
 
+        [Url]
         [Display(Name = "Facebook")]
         public string Facebook { get; set; }
 
+        [Url]
         [Display(Name = "Blog")]
         public string Blog { get; set; }
 
+        [Url]
         [Display(Name = "Website")]
         public string Website { get; set; }
 
+        [MaxLength(100, ErrorMessage = "Your title is too long. Please abbreviate")]
         [Display(Name = "Title")]
         public string Title { get; set; }
 
+        [MaxLength(100, ErrorMessage = "Your profession name is too long. Please abbreviate")]
         [Display(Name = "Profession")]
         public string Profession { get; set; }
          
