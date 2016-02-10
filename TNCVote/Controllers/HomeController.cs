3c7aea6b43d7eb149c1da3a8a6eea419223d7d89
DavidJKelley <pieseczek@hotmail.com>
2016-02-09 23:35:20 -0800

    adding view for send grid testing, add convention view and other UI changes

--- TNCVote/Controllers/HomeController.cs
+++ TNCVote/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SendGrid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -32,8 +34,45 @@ namespace TNCVote.Controllers
             return View();
         }
 
+        public ActionResult Convention()
+        {
+            return View();
+        }
+
         public ActionResult Legal()
         {
+            return View();
+        }
+
+        public ActionResult SendGridTest()
+        {
+            try
+            {
+                // using send grid offline... 
+                //var username = "your_sendgrid_username";
+                //var pswd = "your_sendgrid_password"; 
+                //var credentials = new NetworkCredential(username, pswd);
+                //var transportWeb = new Web(credentials);
+
+                var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+
+                var transportWeb = new Web(apiKey);
+
+                SendGridMessage myMessage = new SendGridMessage();
+                myMessage.From = new MailAddress("donotreply@transhumanpolitics.com");
+                //myMessage.AddTo(NewMemberEmailAccount);
+                myMessage.AddTo("pieseczek@hotmail.com");
+                myMessage.Subject = "Welcome New Registered TNC Member";
+                myMessage.Html = "test";
+
+                transportWeb.DeliverAsync(myMessage);
+            }
+            catch (Exception e)
+            {
+                throw e;
+                //throw (E); 
+            }
+
             return View();
         }
     }
